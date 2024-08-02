259e7d1df9d26ca73f5d4ce557ab38b02e7c5606
Filip Mudri <filipmudri@protonmail.com>
2024-08-02 10:53:06 +0200

    Added a ConnectionString and created the database using Entity Framework

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b7df566118132d6170f5a6f79624e0d0c57571b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b66040899ec44391d1debedf93a0d2eb7e50e7ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
