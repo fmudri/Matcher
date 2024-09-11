3986c84c3f8a85a44882ddc1aaaa51df80932897
Filip Mudri <filipmudri@protonmail.com>
2024-09-11 15:09:49 +0200

    Minor tweaks to frontend

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e51b8588e38b4a75b8eaa18ab9953bc55c93e4aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b2e6173644f7f20013ca7bf6dca2b4e8b9a2112")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
